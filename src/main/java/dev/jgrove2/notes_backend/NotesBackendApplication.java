cbad2f974620fb63daaa5d60f5d17e45f5311f95
Justin <26723005+jgrove2@users.noreply.github.com>
2025-08-04 10:14:12 -0400

    init

---
+++ src/main/java/dev/jgrove2/notes_backend/NotesBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.jgrove2.notes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jgrove2/notes_backend/NotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jgrove2.notes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
