af9c9d9d2159ac896f26eb74ede99a0b2f8acff9
Justin <26723005+jgrove2@users.noreply.github.com>
2025-08-05 18:14:22 -0400

    implement cors headers

---
+++ src/main/java/dev/jgrove2/notes_backend/Config/CustomCorsConfiguration.java
@@ -0,0 +1,21 @@
+package dev.jgrove2.notes_backend.Config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Component
+public class CustomCorsConfiguration implements CorsConfigurationSource {
+    @Override
+    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(
+                List.of("http://localhost:3000", "http://127.0.0.1:3000", "https://notes-alpha.netlify.app/"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        return config;
+    }
+}
